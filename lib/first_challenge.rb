d9719181745f874e52ca5eede9477ecf8bc93447
Irina Bright <missirinabright@gmail.com>
2019-05-31 02:25:46 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
 contacts.["Freddy Mercury"].each do |element, value|
-  if :element == favorite_icecream_flavors
+  if :element == :favorite_icecream_flavors
     value.delete_if do |flavor|
       flavor == "strawberry"
     end
